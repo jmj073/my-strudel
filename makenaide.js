06fb63e4170177f2cddcc55fab26514282bcf029
jmj073 <wjsalswns0733@gmail.com>
2026-03-01 17:19:55 +0900

    modify makenaide

--- makenaide.js
+++ makenaide.js
@@ -1,20 +1,26 @@
 setcpm(30)
+
+let scale = "G:major"
+
 stack(
-    sound("bd hh sd hh hh bd sd hh, hh*8"),
-    n(`
-        <0 4 5 2 3 0 1 4>*2,
-        <2 6 7 4 5 2 3 6>*2,
-        <4 1 2 6 7 4 5 1>*2,
-        <- - - - - - 7 3>*2
-    `)
-        .scale("G:major")
-        .sound("piano"),
+  sound("bd hh sd hh hh bd sd hh"),
+  
+  n("<[0,2,4] [4,6,8] [5,7,2] [2,4,6] [3,5,7] [0,2,4] [1,3,5,7] [4,6,8,3]>")
+    .scale(scale)
+    .s("piano")
+    .lpf(1000),
+
     n(`<
-        [6 4 7 9 8@2 6@2]
-        [7@2 6 5 4@2 4@2]
-        [5@2 4 3 4@2 7@2]
-        [5 7@2 9 8@4]
+      [6 4 7 9]
+      [8@2 6@2]
+      [7@2 6 5]
+      [4@2 4@2]
+      [5@2 4 3]
+      [4@2 7@2]
+      [5 7@2 9]
+      [8@4]
     >`)
-        .scale("G:major")
-        .sound("piano"),
+      .scale(scale)
+      .sound("piano")
+      .gain(2),
 )
